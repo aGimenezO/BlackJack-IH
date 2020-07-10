0d5207c6f98d5594a0457c47427f7a55ed40511e
aGimenezO <65399818+aGimenezO@users.noreply.github.com>
2020-07-10 20:55:00 +0200

    Comments on code, bug fixes

--- venv/Card.py
+++ venv/Card.py
@@ -1,21 +1,29 @@
 class Card:
-    '''Represents a playing card, defined by a value and a suit.'''
+    """Represents a playing card, defined by a value and a suit."""
 
+    @staticmethod
     def get_values():
-        '''This function returns all the values a card can have
+        """This function returns all the values a card can have
             Input: None
-            Output: A list of string containg'''
+            Output: A list of string containg"""
 
         return ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
+    @staticmethod
     def get_suits():
-        ''' Returns all the suits a card can have
+        """ Returns all the suits a card can have
         Input: None
-        Output: A list of strings containing the symbols of the possible suits'''
+        Output: A list of strings containing the symbols of the possible suits"""
 
         return ['♠', '♥', '♣', '♦']
 
     def __init__(self, value, suit):
+        """
+        Creator function of the class Card, described by a value and a suit
+        :param value: String Value of the card, the possible values reassemble the ones in the french deck:
+        ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        :param suit:  String ASCII character containing the suit of the card
+        """
         self.value = value
         self.suit = suit
 
--- venv/CardPool.py
+++ venv/CardPool.py
@@ -1,27 +1,31 @@
+from typing import List
+
 from Card import Card
 from random import shuffle
 
-class CardPool:
 
-    def __init__(self, numberOfDecks=4):
+class CardPool:
+    cards: List[Card]
+
+    def __init__(self, number_of_decks=4):
         self.cards = []
-        for n in range(numberOfDecks):
+        for n in range(number_of_decks):
             for value in Card.get_values():
                 for suit in Card.get_suits():
                     self.cards.append(Card(value, suit))
         self.shuffle_cards()
 
     def shuffle_cards(self):
-        '''Puts the contents in cards in random order
+        """Puts the contents in cards in random order
         Input: None
-        Output: None'''
+        Output: None"""
 
         shuffle(self.cards)
 
     def remove_top_deck(self):
-        '''This method returns the card at the top of the deck
+        """This method returns the card at the top of the deck
             Input: None
-            Output: The card on position 0 of the cards list'''
+            Output: The card on position 0 of the cards list"""
 
         return self.cards.pop(0)
 
